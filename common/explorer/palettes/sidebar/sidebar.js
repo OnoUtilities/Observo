bc8c420b97023b0d413cd48a2fe6148d5905d5fe
Import-Python <Import-Python@users.noreply.github.com>
2017-11-10 19:48:59 -0500

    Made Sidebar Work
    Sidebar now opens and closes! :+1:

--- common/explorer/palettes/sidebar/sidebar.js
+++ common/explorer/palettes/sidebar/sidebar.js
@@ -4,6 +4,17 @@ class Sidebar extends forklift.PaletteBox {
         this.loadBox("elements/o-sidebar/sidebar.shadow.html")
         this.loadContent("elements/o-sidebar/sidebar.html")
     }
+    onContentLoad() {
+        this.contentBox = this.element.querySelector("o-box")
+    }
+    show() {
+        this.contentBox.style.width = "200px"
+        this.contentBox.style.display = ""
+    }
+    hide() {
+        this.contentBox.style.width = "0px"
+        this.contentBox.style.display = "none"
+    }
 }
 
 class View extends forklift.PaletteBox {
@@ -11,6 +22,41 @@ class View extends forklift.PaletteBox {
         super(p)
         this.loadBox("elements/o-sidebar-view/sidebar-view.shadow.html")
         this.loadContent("elements/o-sidebar-view/sidebar-view.html")
+        this.toggle = false
+    }
+    onContentLoad() {
+        let me = this
+        let run = () => {
+            if (me.toggle) {
+                me.show(true)
+            } else {
+                me.show(false)
+            }
+        }
+        this.parent = forklift.App.getPaletteInstance("SIDEBAR").getBoxObject("SIDEBAR")
+        this.element.addEventListener("click", run)
+    }
+    show(data) {
+        if (data == false || data == "false") {
+            this.wait = true
+            this.parent.hide()
+            this.element.setAttribute("show", "false")
+            this.toggle = true
+            this.wait = false
+            this.callEvent("onSidebarResize", [false])
+        } else {
+            this.wait = true
+            this.parent.show()
+            this.element.setAttribute("show", "true")
+            this.toggle = false
+            this.wait = false
+            this.callEvent("onSidebarResize", [true])
+        }
+    }
+    onAttributeChange(name, oldValue, newValue) {
+        if (name == "show" && !this.wait) {
+            this.show(newValue)
+        }
     }
 }
 
